3237914ebd52afe9ba86e031aaa594df81ae4314
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2022-11-16 23:32:20 +0300

    Создание пользователя

--- todolist/core/models.py
+++ todolist/core/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class AbstractUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    age = models.PositiveSmallIntegerField(default=0)
