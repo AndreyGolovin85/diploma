faf8cda16be111b6aee9d8efb896be7bbfb6dacd
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2022-11-16 23:46:42 +0300

    Исправления модели пользователя

--- todolist/core/models.py
+++ todolist/core/models.py
@@ -2,6 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class AbstractUser(AbstractUser):
+class User(AbstractUser):
     email = models.EmailField(unique=True)
     age = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователя"
+
+    def __str__(self):
+        return f"Пользователь: {self.first_name} {self.last_name}"
