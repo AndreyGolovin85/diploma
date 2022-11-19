0edee091c6d23d72bdacc8baf338ea521b51a70d
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2022-11-20 00:10:25 +0300

    Создание и подключение к базе

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': env.str('POSTGRES_HOST'),
+        'PORT': env.str('POSTGRES_PORT'),
     }
 }
 
