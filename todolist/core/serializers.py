b9f7f2eed2be3a533be0602a8f78e9f85a020500
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2022-12-04 23:03:16 +0300

    Проверка паролей в сериалайзере.
    Проверяем совпадают ли пароли при регистрации и хэшируем пароль.

---
+++ todolist/core/serializers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+
+from models import User
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_repeat = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        password = validated_data.get("password")
+        password_repeat = validated_data.pop("password_repeat")
+
+        if password != password_repeat:
+            raise serializers.ValidationError("Пароли не совпадают")
+
+        hashed_password = make_password(password)
+        validated_data["password"] = hashed_password
+        instance = super().create(validated_data)
+        return instance
+
+    class Meta:
+        model = User
+        fields = "__all__"
