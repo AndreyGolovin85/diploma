7827f25b8178cf99571b50ce13246198efb98e56
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2022-12-07 23:30:47 +0300

    Update urls.py

--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup', views.RegistrationView.as_view(), name='signup'),
-    path('login', views.LoginView.as_view(), name='login'),
-    path('profile', views.ProfileView.as_view(), name='profile'),
-    path('update_password', views.PasswordUpdateView.as_view(), name='update_password'),
+    path('core/signup', views.RegistrationView.as_view(), name='signup'),
+    path('core/login', views.LoginView.as_view(), name='login'),
+    path('core/profile', views.ProfileView.as_view(), name='profile'),
+    path('core/update_password', views.PasswordUpdateView.as_view(), name='update_password'),
 ]
