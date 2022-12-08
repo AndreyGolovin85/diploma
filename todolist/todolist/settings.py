928901817e8c39d4b0332e62c6dbb885e4614584
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2022-12-08 14:35:06 +0300

    Update settings.py

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -33,7 +33,7 @@ environ.Env.read_env(ENV_FILE_PATH)
 SECRET_KEY = env.str('SECRET_KEY', 'secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', default='True')
 
 ALLOWED_HOSTS = ["*"]
 
